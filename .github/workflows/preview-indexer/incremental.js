b4046c3cb8d7af49286b9be439db6921e462779b
Raghu A <raga@raghus-mbp-2.corp.adobe.com>
2025-11-27 13:25:46 +0530

    Test runs

--- .github/workflows/preview-indexer/incremental.js
+++ .github/workflows/preview-indexer/incremental.js
@@ -3,7 +3,7 @@ import { getLingoConfigMap } from './internal/utils.js';
 import { initIndexer } from './internal/indexer.js';
 
 // Add delay of 90 seconds before processing begins
-await new Promise(resolve => setTimeout(resolve, 360 * 1000));
+await new Promise(resolve => setTimeout(resolve, 600 * 1000));
 
 const ORG = 'adobecom';
 
